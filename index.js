646b0fc0038d59a0434f25a43f1d9d2be60c4900
Manuel Saez Carmona <70754764+manuelsaezcarmona@users.noreply.github.com>
2022-03-27 12:38:05 +0200

    add route auth.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 // Vamos a usar para el backEnd la importacion clasica con require.
 // Si quieres hacerlo con molulos import / export . Puedes hacerlo poniendo
 // en el package.json la siguiente linea: "type": "module",
@@ -16,9 +17,11 @@ const app = express();
 app.use(express.static('public'));
 
 // Rutas
-/* app.get('/', (req, res) => {
-  res.json({ ok: true });
-}); */
+// este comando lo que quiere decir es que todo lo que se va a importar en este archivo lo va a habilitar en esta ruta
+app.use('/api/auth', require('./routes/auth'));
+
+// TODO: Auth // crear usuarios, login,  renew con token.
+// TODO: CRUD -> Eventos
 
 // Puesta en marcha . escuchar peticiones
 app.listen(process.env.PORT, () => {
---
+++ routes/auth.js
@@ -0,0 +1,14 @@
+/**
+ * Rutas de usuarios / Auth
+ *  host + /api/auth
+ */
+
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.json({ ok: true });
+});
+
+module.exports = router;
