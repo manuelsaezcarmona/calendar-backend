19179be88a5a54be45d00f2fdee8be28e17b87f1
Manuel Saez Carmona <70754764+manuelsaezcarmona@users.noreply.github.com>
2022-03-29 14:38:25 +0200

    add eliminarevento method to events.controller.js

--- controllers/events.controller.js
+++ controllers/events.controller.js
@@ -48,7 +48,7 @@ const actualizarEvento = async (req, res = response) => {
     const evento = await Evento.findById(eventId);
 
     if (!evento) {
-      res.status(404).json({
+      return res.status(404).json({
         ok: false,
         msg: 'No existe Evento con ese ID',
       });
@@ -95,12 +95,44 @@ const actualizarEvento = async (req, res = response) => {
   }
 };
 
-const eliminarEvento = (req, res = response) => {
-  res.status(200).json({
-    id: req.id,
-    ok: true,
-    msg: 'evento eliminado',
-  });
+const eliminarEvento = async (req, res = response) => {
+  const eventId = req.params.id;
+  const { uid } = req;
+
+  try {
+    // Veificar que el elemento exista
+    const evento = await Evento.findById(eventId);
+
+    // SI no existe el evento
+    if (!evento) {
+      return res.status(404).json({
+        ok: false,
+        msg: 'No existe Evento con ese ID',
+      });
+    }
+
+    // Si el usuario no es el mismo que lo creo
+    if (evento.user.toString() !== uid) {
+      return res.status(401).json({
+        ok: false,
+        msg: 'No tiene permiso para editar este evento',
+      });
+    }
+    // Pasadas las validaciones procedo al borrado
+    const eventoBorrado = await Evento.findByIdAndDelete(eventId);
+
+    res.status(200).json({
+      ok: true,
+      msg: 'Evento eliminado',
+      title: eventoBorrado.title,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: 'Error al borrar el evento',
+    });
+  }
 };
 
 module.exports = {
